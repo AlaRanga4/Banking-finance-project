9736fb04fda28eadf3eb6376fe817a8a8cfbb3bd
aravindrevalli59 <120809180+aravindrevalli59@users.noreply.github.com>
2023-04-29 19:24:49 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0c768662cc797cd75" 
   instance_type = "t2.micro" 
-  key_name = "newkey"
-  vpc_security_group_ids= ["sg-0315c97a013200234"]
+  key_name = "kittu-key"
+  vpc_security_group_ids= ["sg-0025765b8d7b2983e"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    #private_key = file("./newkey.pem")
-    private_key = file("newkey.pem")
+    #private_key = file("./kittu-key.pem")
+    private_key = file("kittu-key.pem")
     timeout = "3m"
     agent = false
     host     = self.public_ip
@@ -22,6 +22,6 @@ resource "aws_instance" "test-server" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/finance-project01/my-serverfiles/finance-playbook.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/finance/my-serverfiles/finance-playbook.yml "
   } 
 }
