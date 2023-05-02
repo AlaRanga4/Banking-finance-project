1b0ab42f18debb142303acfe0064450f5681a3e3
aravindrevalli59 <120809180+aravindrevalli59@users.noreply.github.com>
2023-05-02 15:00:10 +0530

    Update provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region     = "ap-south-1" 
-  access_key = "AKIAUJ7YUJO2EFOSSLWW"
-  secret_key = "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"
+  access_key = "     "
+  secret_key = "     "
 }
