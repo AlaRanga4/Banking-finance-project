442fb9497b77a4cb176270dca35910676510a8ff
Dineshdevop1 <88160735+Dineshdevop1@users.noreply.github.com>
2023-05-09 09:42:53 +0530

    Update provider.tf
    key added

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-region     = "ap-south-1" 
-  access_key = "     "
-  secret_key = "     "
+region     = "us-east-1" 
+  access_key = "AKIA25AG3NKH2RP2RVXJ"
+  secret_key = "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"
 }
